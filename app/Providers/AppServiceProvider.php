975b091de542d22ea8040628b058c5a0bbf2cafd
山本晃司 <koji.19@icloud.com>
2026-01-13 15:32:57 +0900

    deploy to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
